2f1317fa8113bac12c163911d7195f43710b78bd
Nanda Marri <nanda.marri@gmail.com>
2025-08-20 12:23:05 +0000

    Convert from Gosu to Java for Gradle 9.0 compatibility

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+package main.java;
+
+/**
+ * Simple Calculator class for demonstration
+ */
+public class Calculator {
+  
+  public int add(int a, int b) {
+    return a + b;
+  }
+  
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+  
+  public int multiply(int a, int b) {
+    return a * b;
+  }
+  
+  public double divide(int a, int b) {
+    if (b == 0) {
+      throw new IllegalArgumentException("Division by zero");
+    }
+    return (double) a / b;
+  }
+  
+  public boolean isEven(int number) {
+    return number % 2 == 0;
+  }
+}
